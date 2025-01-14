55da873257d4ba4e8e20f09e9b8b5e3e91d40edb
erizo <nandezemir@gmail.com>
2025-01-13 23:35:01 -0600

    Regresamos del viaje

--- IntroduccionJS/js/10-Funciones.js
+++ IntroduccionJS/js/10-Funciones.js
@@ -9,7 +9,7 @@ sumar(100, 146)
 sumar(10)
 
 
-//Funciones - Funciones Expression
+Funciones - Funciones Expression
 const sumar = function(numero1 = 0, numero2 = 0){
     console.log(numero1 + numero2)
 }
@@ -17,13 +17,20 @@ const sumar = function(numero1 = 0, numero2 = 0){
 sumar(10, 20)
 sumar(100, 200)
 sumar(685)
-*/
 
-//Arrow functions
+
+Arrow functions
 const sumar = (numero1 = 0, numero2 = 0) => console.log(numero1 + numero2)
 
 sumar(10, 28)
 sumar(300, 3)
 sumar(100)
+*/
 
 //Funciones que retornan valores 
+const sumar = function(numero1 = 0, numero2 = 0){
+    console.log(numero1 + numero2)
+}
+
+
+sumar(23,3)
