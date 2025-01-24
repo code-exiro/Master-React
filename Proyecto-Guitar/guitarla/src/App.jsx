c4db4f12b4ead116d273fef317f5d3fddb6244bb
erizo <nandezemir@gmail.com>
2025-01-24 12:55:41 -0600

    Carrito de compras

--- Proyecto-Guitar/guitarla/src/App.jsx
+++ Proyecto-Guitar/guitarla/src/App.jsx
@@ -1,10 +1,58 @@
-import { useState, useEffect} from 'react';
+import { useState } from 'react';
 import  Header  from './components/Header';
 import Guitar from './components/Guitar';
+import { db } from './data/db'
+
+function App() {
+
+    const [data, setData] = useState(db)
+    const [cart , setCart] = useState([]) 
+
+    function addToCart(item) {
+
+        const itemExists = cart.findIndex(guitar => guitar.id === item.id)
+        if(itemExists >= 0) { // Existe en el carrito
+            console.log('Ya existe...')
+        } else {
+            item.quantity =1 
+            setCart(prevCart => [...prevCart, item])
+        }
+
+    }
+
+    return (
+        <>
+
+        <Header />
+
+        <main className='container-xl mt-5'>
+            <h2 className='text-center'>Nuestra Coleccion</h2>
+
+            <div className='row mt-5'>
+                {data.map((guitar) => (
+                    <Guitar
+                        key={guitar.id}
+                        guitar={guitar}
+                        setCart={setCart}
+                        addToCart={addToCart}
+                    />
+                ))}
+
+            </div>
+        </main>
+
+        <footer className='bg-dark mt-5 py-5'>
+            <div className='container-xl'>
+                <p className='text-white text-center fs-4 mt-4 m-md-0'>GuitarLA - Todos los derechos Reservados</p>
+            </div>
+        </footer>
+        </>
+    )
+}
 
 export default App
 
-function App() {
+//Hooks
 
     /* State
     const [auth, setAuth] = useState(false)
@@ -27,31 +75,3 @@ function App() {
     setTimeout(() => {
         setAuth(true)
     }, 3000);*/
-
-    return (
-        <>
-
-        <Header />
-
-        <main className='container-xl mt-5'>
-            <h2 className='text-center'>Nuestra Coleccion</h2>
-
-            <div className='row mt-5'>
-                <Guitar/>
-                <Guitar/>
-                <Guitar/>
-                <Guitar/>
-                <Guitar/>
-                <Guitar/>
-            </div>
-        </main>
-
-        <footer className='bg-dark mt-5 py-5'>
-            <div className='container-xl'>
-                <p className='text-white text-center fs-4 mt-4 m-md-0'>GuitarLA - Todos los derechos Reservados</p>
-            </div>
-        </footer>
-
-        </>
-    )
-}
